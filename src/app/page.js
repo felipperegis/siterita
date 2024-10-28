98b8452594fdfe72dafc080cb8da5fbc3ab7b84b
Felippe <felippesantos90@hotmail.com>
2024-10-27 22:52:56 -0300

    pagina inicial 2

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,16 @@ import Footer from "./paginas/Footer"
 import MeiodaPagina from "./paginas/MeiodaPagina"
 import Bolo from "./paginas/Imagem/bolo.png"
 import Bolo1 from "./paginas/Imagem/bolo1.png"
+import Image from "next/image"
+import cabecalho from "./Styles/cabecalho.css"
 
 export default function Index (){
   return (
-      <div>
+      <div className="">
         <Header/>
         <MeiodaPagina/>
-        <Bolo/>
-        <Bolo1/>
+        <Image className="bolo" src={Bolo}/>
+        <Image className="bolo" src={Bolo1}/>
         <Footer/>
       </div> )
 }
