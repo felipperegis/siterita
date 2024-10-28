fcf1c7f8f5f3148fe60dc499f8d6c1d5150e475a
Felippe <felippesantos90@hotmail.com>
2024-10-27 22:27:48 -0300

    add bolo

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Header from "./paginas/Header"
 import Footer from "./paginas/Footer"
 import MeiodaPagina from "./paginas/MeiodaPagina"
-
+import Bolo from "./paginas/Imagem/bolo.png"
+import Bolo1 from "./paginas/Imagem/bolo1.png"
 
 export default function Index (){
   return (
       <div>
         <Header/>
         <MeiodaPagina/>
+        <Bolo/>
+        <Bolo1/>
         <Footer/>
       </div> )
 }
