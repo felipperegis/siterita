3cf0263513de166efe42b8b79b1d38a0d5020640
Felippe <felippesantos90@hotmail.com>
2024-11-12 23:57:27 -0300

    adicionando foto e texto inicial

--- src/app/quemsomos/page.js
+++ src/app/quemsomos/page.js
@@ -2,14 +2,24 @@ import Header from "../paginas/Header"
 import Rita from "../paginas/Imagem/rita.jpg"
 import Image from "next/image"
 import "../Styles/Quemsomos.css"
+
+
 export default function quemsomos () {
   return(
     < >
       <Header/> 
+      <div>
+        <div className="layoutquemsomos">
         <Image src= {Rita} className="RitaFotoQuemSomos"/>      
-        <h1>oi</h1>
-        
-      
+        <h1 className="descriçãoprimeiroparagrafo">Sobre Mim
+
+Me chamo Rita de Cassia Straub Cavalheiro e sou apaixonada por gastronomia. Formada na área, especializei-me na criação de doces que encantam não só pelo sabor, mas também pela apresentação impecável.
+
+Meu foco principal é confeccionar delícias como brigadeiros e bolos que transformam qualquer ocasião em um evento especial. Cada doce é feito com ingredientes selecionados e uma dose extra de carinho, garantindo que cada mordida seja uma experiência inesquecível.
+
+Permita-me adoçar seus momentos especiais com minhas criações. Vamos juntos transformar suas comemorações em memórias deliciosas e inesquecíveis.</h1>
+      </div>
+    </div>  
     </>
   )
 } 
