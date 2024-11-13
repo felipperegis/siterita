e7c3690f89f209598276d308298b33ad88b42813
Felippe <felippesantos90@hotmail.com>
2024-11-12 23:36:59 -0300

    add quem somos

--- src/app/quemsomos/page.js
+++ src/app/quemsomos/page.js
@@ -1,11 +1,12 @@
 import Header from "../paginas/Header"
-
-
-
+import Rita from "../paginas/Imagem/rita.jpg"
+import Image from "next/image"
+import "../Styles/Quemsomos.css"
 export default function quemsomos () {
   return(
     < >
-      <Header/>      
+      <Header/> 
+        <Image src= {Rita} className="RitaFotoQuemSomos"/>      
         <h1>oi</h1>
         
       
