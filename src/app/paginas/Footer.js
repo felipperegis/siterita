09f5e2d24a543ed5911778de6d96d79c117d80e9
Felippe <felippesantos90@hotmail.com>
2024-11-27 23:34:43 -0300

    cartões

--- src/app/paginas/Footer.js
+++ src/app/paginas/Footer.js
@@ -1,10 +1,18 @@
 
+import Image from "next/image"
+import visa from "../paginas/Imagem/visa.png"
+import alelo from "../paginas/Imagem/alelo.png"
+import mastercard from "../paginas/Imagem/mastercard.png"
 import "../Styles/Footer.css"
 
 
 export default function Footer () {
     return(
     <div className="Footer">
+    <p > Aceitamos os cartões .</p>
+    <Image src={visa} className="VisaFooter"/>
+    <Image src={mastercard} className="MastercardFooter"/>
+    <Image src={alelo} className="AleloFooter"/>
     <p clas>Copyright © </p>
     </div>
     )
