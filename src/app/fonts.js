71b6a13db55bea5bc42d3c4569aa5d839b4c05c0
Felippe <felippesantos90@hotmail.com>
2024-12-27 18:43:17 -0300

    adicionando fonte e alterando

---
+++ src/app/fonts.js
@@ -0,0 +1,19 @@
+import { Inter, Roboto_Mono, EB_Garamond, Cormorant_Garamond } from 'next/font/google'
+ 
+export const inter = Inter({
+  weight: ["600", "500"],
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  weight: ["600", "700"],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const garamond = Cormorant_Garamond ({
+  weight: ["700", "700"],
+  subsets: ['latin'],
+  display: 'swap',
+})
--- src/app/paginas/Header.js
+++ src/app/paginas/Header.js
@@ -1,13 +1,16 @@
 import Link from "next/link"
 import Image from "next/image"
-import cabecalho from "../Styles/cabecalho.css"
-import HomeHeader from "../Styles/Header.css"
+import "../Styles/cabecalho.css"
 import IconeHome from "../paginas/Imagem/IconeHome.svg"
 import QuemSomos from "../paginas/Imagem/QuemSomos.svg"
 import OndeEstamos from "../paginas/Imagem/OndeEstamos.svg"
 import FaleConosco from "../paginas/Imagem/FaleConosco.svg"
 import Produtos from "../paginas/Imagem/Produtos.svg"
-import Roboto from "../paginas/fonts"
+
+
+import { garamond } from "../fonts"
+
+
 
 
 
@@ -16,14 +19,14 @@ export default function Header ({ Component, pageProps }) {
  
   return( <>
     <div >
-      <nav  >
-        <ul className= "cabecalho {Roboto.ClassName}">
-          
-          <li> <Link href={'/'}> INICIO   <Image src={IconeHome} className="px-6 inline" /> </Link>  </li>
-          <li> <Link href="/quemsomos">QUEM SOMOS <Image src={QuemSomos} className="px-5 inline" /> </Link>  </li>
-          <li> <Link href={"/ondeestamos"}> ONDE ESTAMOS <Image src={OndeEstamos} className="px-5 inline" /> </Link></li>
-          <li> <Link href={"/faleconosco "}> FALE CONOSCO <Image src={FaleConosco} className="px-5 inline " /> </Link></li>
-          <li> <Link href={"/produtos"}>  </Link> PRODUTOS <Image src={Produtos} className="px-5 inline" /></li>
+      <nav >
+        <ul className= "cabecalho" >
+        
+          <li className= {garamond.className} > <Link href={'/'}  > INICIO   <Image src={IconeHome} className="px-6 inline" /> </Link>  </li>
+          <li className= {garamond.className} > <Link href="/quemsomos">QUEM SOMOS <Image src={QuemSomos} className="px-5 inline" /> </Link>  </li>
+          <li className= {garamond.className} > <Link href={"/ondeestamos"}> ONDE ESTAMOS <Image src={OndeEstamos} className="px-5 inline" /> </Link></li>
+          <li className= {garamond.className} > <Link href={"/faleconosco "}> FALE CONOSCO <Image src={FaleConosco} className="px-5 inline " /> </Link></li>
+          <li className= {garamond.className} > <Link href={"/produtos"}>  </Link> PRODUTOS <Image src={Produtos} className="px-5 inline" /></li>
         </ul>
         
       </nav>
