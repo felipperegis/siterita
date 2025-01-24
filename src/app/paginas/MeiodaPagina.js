97467cf65baa3c60f6dadb23248ba863c9fc02ec
Felippe <felippesantos90@hotmail.com>
2025-01-23 23:22:44 -0300

    arrumando classname para id

--- src/app/paginas/MeiodaPagina.js
+++ src/app/paginas/MeiodaPagina.js
@@ -1,14 +1,20 @@
 
 import Image from "next/image"
 import boloInicial from "./Imagem/boloinicial.png"
+import rita from "./Imagem/rita.jpg"
 import  "../Styles/MeiodaPagina.css"
 
 
 export default function MeiodaPagina (){
     return (
-        <div className="MeiodaPagina">
-            <Image src={boloInicial} className="boloInicial"/>
+        <>
+        <div id="MeiodaPagina">
+            <Image src={boloInicial} id="boloInicial"/>
             
         </div>
+        <div>
+        <Image src={rita} id="RitaMeiodaPagina"/>
+        </div>
+        </>
     );
 }
